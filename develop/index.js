209cd8848fdbcafdd0022232d27e82f3ff22b182
EJRobbins <ericarobbins00@gmail.com>
2020-05-16 19:43:23 -0600

    json and index javascript

---
+++ develop/index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const getApi = require("./utilities/api");
+const markDown = require("./utilities/generateMarkdown");
+
+
+const questions = [
+    {
+        type: "input",
+        name: "git",
+        message: "Please enter your Github username"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project title"
+    },
+    {
+        type: "input",
+        name: "proDec",
+        message: "Please enter a description of your project"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your droject require?",
+        choices: ["GPL 3.0", "MIT", "Apache 2.0", "none"]
+    },
+    {
+        type: "input",
+        name: "instDep",
+        message: "Command to install dependencies?",
+        default: "npm install"
+    },
+    {
+        type: "input",
+        name: "useage",
+        message: "Please enter anything the user needs to know for this repo?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "Please enter any information the user needs to know about contributing."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "what commands do we need to run a test.",
+        default: "npm test"
+    }
+
+];
+
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then((responses) => {
+        getApi.getUser(responses.git)
+        .then(({data}) => {
+            writeToFile("ReadMe.md", markDown({...responses, ...data}));
+        });
+    });
+}
+
+init();
